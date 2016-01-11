2bcd3e730dae8348a2fc7eaa44306d7907be496b
maksimgm <maksimmamrikov@gmail.com>
2016-01-11 14:42:01 -0800

    add gmap and breeze api calls

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 var app = angular.module("EssentialAir", ['ngRoute']);
+// API KEY--- AIzaSyBn7lMGzul-f5i72BrVHNgI8F12PDKvSOQ
+app.directive('myMaps', function(){
+  return{
+    restruct:'E',
+    template: "<div></div>",
+    replace: true,
+    link: function(scope,element,attrs){
+      var myLatLng = new google.maps.LatLng(40.7324296, -73.9977264);
+      var mapOptions  = {
+        center: myLatLng,
+        zoom: 16,
+        // Default is ROADMAP
+        mapTypeId: google.maps.MapTypeId.HYBRID
+      };
+      var map = new google.maps.Map(document.getElementById(attrs.id),
+        mapOptions);
+      var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: "Home"
+      });
+      marker.setMap(map);
+    }
+  };
+});
--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,13 @@
-app.controller("MainController", function($scope, $http, $rootScope){
-  // api key... 1827fecc1c064b05b2e2d07f90961a74
-  var url = "https://api.breezometer.com/baqi/?lat=40.7324296&lon=-73.9977264&key=1827fecc1c064b05b2e2d07f90961a74";
-    $http.get(url).then(function(data){
-      $scope.data = data;
-      console.log(data);
-  });
-});
+// app.controller("MainController", function($scope, $http, $rootScope){
+//   // api key... 1827fecc1c064b05b2e2d07f90961a74
+//   var url = "https://api.breezometer.com/baqi/?lat=40.7324296&lon=-73.9977264&key=1827fecc1c064b05b2e2d07f90961a74";
+//     $http.get(url).then(function(data){
+//       $scope.data = data;
+//       console.log(data);
+//   });
+
+    // Adds autocomplete to trip form location text field
+//GOOGLE MAPS AUTO COMPLETE API = AIzaSyDRtPelzvMVwlTwyQuhN-goYUByFUVNlIo
+
+
+// });
