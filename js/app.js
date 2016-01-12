3f65a276bc972dec5f8f89d02c1961204c38c8b4
maksimgm <maksimmamrikov@gmail.com>
2016-01-11 22:15:51 -0800

    attempt add geocoding and map

--- js/app.js
+++ js/app.js
@@ -1,26 +1,40 @@
-var app = angular.module("EssentialAir", ['ngRoute']);
-// API KEY--- AIzaSyBn7lMGzul-f5i72BrVHNgI8F12PDKvSOQ
-app.directive('myMaps', function(){
-  return{
-    restruct:'E',
-    template: "<div></div>",
-    replace: true,
-    link: function(scope,element,attrs){
-      var myLatLng = new google.maps.LatLng(40.7324296, -73.9977264);
-      var mapOptions  = {
-        center: myLatLng,
-        zoom: 16,
-        // Default is ROADMAP
-        mapTypeId: google.maps.MapTypeId.HYBRID
-      };
-      var map = new google.maps.Map(document.getElementById(attrs.id),
-        mapOptions);
-      var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: "Home"
-      });
-      marker.setMap(map);
-    }
-  };
+var app = angular.module("EssentialAir", ['angularReverseGeocode']);
+
+
+
+
+app.directive('reverseGeocode', function () {
+    return {
+      restrict: 'E',
+      template: '<div></div>',
+      link: function (scope, element, attrs) {
+          var geocoder = new google.maps.Geocoder();
+          var LatLng = new google.maps.LatLng(attrs.lat, attrs.lng);
+          geocoder.geocode({ 'latLng': LatLng }, function (results, status) {
+              if (status == google.maps.GeocoderStatus.OK) {
+                  if (results[1]) {
+                      element.text(results[1].formatted_address);
+                  } else {
+                      element.text('Location not found');
+                  }
+              } else {
+                  element.text('Geocoder failed due to: ' + status);
+              }
+          });
+          var mapOptions  = {
+            center: myLatLng,
+            zoom: 16,
+          // Default is ROADMAP
+            mapTypeId: google.maps.MapTypeId.HYBRID
+          };
+          var map = new google.maps.Map(document.getElementById(attrs.id),mapOptions);
+          var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: "Home"
+          });
+          marker.setMap(map);
+        },
+      replace: true
+    };
 });
--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,4 @@
+// breezometer API call
 // app.controller("MainController", function($scope, $http, $rootScope){
 //   // api key... 1827fecc1c064b05b2e2d07f90961a74
 //   var url = "https://api.breezometer.com/baqi/?lat=40.7324296&lon=-73.9977264&key=1827fecc1c064b05b2e2d07f90961a74";
@@ -11,3 +12,73 @@
 
 
 // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.controller('geolocCtrl', ['$scope', function($scope) {
+
+//   var geocoder;
+//   var map;
+//   $scope.initialize = function(){
+//       geocoder = new google.maps.Geocoder();
+//       map = new google.maps.Map(document.getElementById("map"),
+//       {
+//           zoom: 8,
+//           center: new google.maps.LatLng(22.7964,79.5410),
+//           mapTypeId: google.maps.MapTypeId.ROADMAP
+//       });
+//   };
+
+//   $scope.codeAddress = function()
+//   {
+//       var address = document.getElementById("address").value;
+//       geocoder.geocode( { 'address': address}, function(results, status)
+//       {
+//           if (status == google.maps.GeocoderStatus.OK)
+//           {
+//               map.setCenter(results[0].geometry.location);
+//               var marker = new google.maps.Marker(
+//               {
+//                   map: map,
+//                   position: results[0].geometry.location
+//               });
+//           }
+//           else
+//           {
+//               alert("Geocode was not successful for the following reason: " + status);
+//           }
+//       });
+//   };
+// }]);
