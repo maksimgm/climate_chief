02145561a602127369a008691f683944880b25f4
maksimgm <maksimmamrikov@gmail.com>
2016-01-14 15:23:41 -0800

    add autocompelte

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -14,8 +14,22 @@ app.controller('MainController', function($scope, $http) {
         $scope.showModal = !$scope.showModal;
     };
 
+  // var geocoder = new google.maps.Geocoder();
+  // $scope.geoFun = function() {
+  //   geocoder.geocode( { "address": $scope.address }, function(results, status) {
+  //       if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
+  //           $scope.lat = results[0].geometry.location.lat();
+  //           $scope.lng = results[0].geometry.location.lng();
+  //           initMap();
+  //           breezeData();
+  //           // call breeze function to get access to 
+  //       }else if(status !== google.maps.GeocoderStatus.OK){
+  //         alert("That Information is not available.");
+  //       }
+  //   });
+  // };
 
-  breezeData = function(){
+  var breezeData = function(){
     var url = "https://api.breezometer.com/baqi/?lat="+ $scope.lat +"&lon="+ $scope.lng +"&key=1827fecc1c064b05b2e2d07f90961a74";
     $http.get(url).then(function(data){
       $scope.data = {
@@ -28,69 +42,54 @@ app.controller('MainController', function($scope, $http) {
     });
   };
 
-  var geocoder = new google.maps.Geocoder();
-  $scope.geoFun = function() {
-    geocoder.geocode( { "address": $scope.address }, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-            $scope.lat = results[0].geometry.location.lat();
-            $scope.lng = results[0].geometry.location.lng();
-            initMap();
-            breezeData();
-            // call breeze function to get access to 
-        }else if(status !== google.maps.GeocoderStatus.OK){
-          alert("That Information is not available.");
-        }
-    });
-  };
-
-
-  initMap = function() {
-    // Create a map object and specify the DOM element for display.
+  $scope.initMap = function() {
     var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: $scope.lat, lng: $scope.lng},
-      scrollwheel: false,
-      zoom: 13,
-      mapTypeId: google.maps.MapTypeId.HYBRID
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13
+    });
+    var input = /** @type {!HTMLInputElement} */(document.getElementById('pac-input'));
+    // var submit = /** @type {!HTMLInputElement} */(document.getElementsByByClassName('submitButton'));
+    var types = document.getElementById('type-selector');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+      map: map,
+      anchorPoint: new google.maps.Point(0, -29)
     });
 
-    // var input = document.getElementById('searchTextField');
+    autocomplete.addListener('place_changed', function() {
+      infowindow.close();
+      marker.setVisible(false);
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
 
-    //   var autocomplete = new google.maps.places.Autocomplete(input);
-    //   autocomplete.bindTo('bounds', map);
-
-    //   var infowindow = new google.maps.InfoWindow();
-      
-
-    // autocomplete.addListener('place_changed', function() {
-    //   infowindow.close();
-    //   // marker.setVisible(false);
-    //   var place = autocomplete.getPlace();
-    //   if (!place.geometry) {
-    //     window.alert("Autocomplete's returned place contains no geometry");
-    //     return;
-    //   }
-
-    //   // If the place has a geometry, then present it on a map.
-    //   if (place.geometry.viewport) {
-    //     map.fitBounds(place.geometry.viewport);
-    //   } else {
-    //     map.setCenter(place.geometry.viewport);
-    //     map.setZoom(17);  // Why 17? Because it looks good.
-    //   }
-
-    //   var address = '';
-    //   if (place.address_components) {
-    //     address = [
-    //       (place.address_components[0] && place.address_components[0].short_name || ''),
-    //       (place.address_components[1] && place.address_components[1].short_name || ''),
-    //       (place.address_components[2] && place.address_components[2].short_name || '')
-    //     ].join(' ');
-    //   }
-    // });
-    
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(16);  // Why 16? Because it looks good. :)
+      }
+       if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+      $scope.lat = place.geometry.location.lat();
+      $scope.lng = place.geometry.location.lng();
+      breezeData();
+      // CHECK TO SEE IF BREEZEOMETER DATA IS AVAILABLE. IF IT IS NOT AVAILABLE THEN RETURN A FLASH MESSAGE.
+    });
   };
-            // initMap();
-            // breezeData();
+  $scope.initMap();
+
 });
 
 
