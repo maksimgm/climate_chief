35493d69ca6e8b7d41a363ad77bd1c9b2eec93c9
maksimgm <maksimmamrikov@gmail.com>
2016-01-14 13:28:41 -0800

    add style to page

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -24,21 +24,9 @@ app.controller('MainController', function($scope, $http) {
         dominantPollutant: data.data.dominant_pollutant_description,
         pollutionEffects: data.data.dominant_pollutant_text.effects
       };
-      
+
     });
   };
-    // air quality index
-    // $scope.data.airQualityIndex = data.data.breezometer_aqi ;
-    // var airQualityIndex = $scope.data.breezometer_aqi;
-    
-    // console.log(data.data.breezometer_description);
-    // console.log(data.data.random_recommendations.health);
-    // console.log(data.data.dominant_pollutant_description);
-    // console.log(data.data.dominant_pollutant_text.effects);
-
-  
-
-
 
   var geocoder = new google.maps.Geocoder();
   $scope.geoFun = function() {
@@ -48,21 +36,61 @@ app.controller('MainController', function($scope, $http) {
             $scope.lng = results[0].geometry.location.lng();
             initMap();
             breezeData();
-            // call breez function to get access to 
+            // call breeze function to get access to 
+        }else if(status !== google.maps.GeocoderStatus.OK){
+          alert("That Information is not available.");
         }
     });
   };
+
 
   initMap = function() {
     // Create a map object and specify the DOM element for display.
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: $scope.lat, lng: $scope.lng},
-      // add details about the type of map...HYBRID
-      // add marker
       scrollwheel: false,
-      zoom: 13
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.HYBRID
     });
+
+    // var input = document.getElementById('searchTextField');
+
+    //   var autocomplete = new google.maps.places.Autocomplete(input);
+    //   autocomplete.bindTo('bounds', map);
+
+    //   var infowindow = new google.maps.InfoWindow();
+      
+
+    // autocomplete.addListener('place_changed', function() {
+    //   infowindow.close();
+    //   // marker.setVisible(false);
+    //   var place = autocomplete.getPlace();
+    //   if (!place.geometry) {
+    //     window.alert("Autocomplete's returned place contains no geometry");
+    //     return;
+    //   }
+
+    //   // If the place has a geometry, then present it on a map.
+    //   if (place.geometry.viewport) {
+    //     map.fitBounds(place.geometry.viewport);
+    //   } else {
+    //     map.setCenter(place.geometry.viewport);
+    //     map.setZoom(17);  // Why 17? Because it looks good.
+    //   }
+
+    //   var address = '';
+    //   if (place.address_components) {
+    //     address = [
+    //       (place.address_components[0] && place.address_components[0].short_name || ''),
+    //       (place.address_components[1] && place.address_components[1].short_name || ''),
+    //       (place.address_components[2] && place.address_components[2].short_name || '')
+    //     ].join(' ');
+    //   }
+    // });
+    
   };
+            // initMap();
+            // breezeData();
 
 
 });
