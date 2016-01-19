4086d68aa46f7e24bca5b1a9b0783cfdb5fbde6f
maksimgm <maksimmamrikov@gmail.com>
2016-01-19 15:32:22 -0800

    add fb authentication

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,66 @@
-var app = angular.module("EssentialAir", []);
+var app = angular.module("EssentialAir", ['ngRoute', 'satellizer']);
+
+
+app.config(function($routeProvider, $locationProvider, $authProvider){
+  $routeProvider
+  .when('/home', {
+    controller: "MainController",
+    templateUrl: "templates/index.html",
+    resolve: {
+      loginRequired: loginRequired
+    }
+  })
+  .when('/', {
+    controller: "LoginController",
+    templateUrl: "templates/login.html",
+    resolve: {
+      skipIfLoggedIn: skipIfLoggedIn
+    }
+  })
+  .when('/signup', {
+    controller: "SignupController",
+    templateUrl: "templates/signup.html",
+    resolve: {
+      skipIfLoggedIn: skipIfLoggedIn
+    }
+  })
+  .when('/logout', {
+    template: null,
+    controller: 'LogoutController'
+  })
+  .otherwise({redirectTo:'/'});
+
+  $locationProvider.html5Mode(true);
+
+  $authProvider.facebook({
+    clientId: '1684535625138001',
+    url: '/api/auth/facebook'
+  });
+
+  function skipIfLoggedIn($q, $auth, $location) {
+      var deferred = $q.defer();
+      if ($auth.isAuthenticated()) {
+        $location.path('/home');
+        deferred.reject();
+      } else {
+        deferred.resolve();
+      }
+      return deferred.promise;
+    }
+
+    function loginRequired($q, $location, $auth) {
+      var deferred = $q.defer();
+      if ($auth.isAuthenticated()) {
+        deferred.resolve();
+      } else {
+        $location.path('/');
+      }
+      return deferred.promise;
+    }
+  
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+
+});
