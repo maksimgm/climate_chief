42d2d0f73f67f11b0e2aa3f59ff41aff2f6c4f73
maksimgm <maksimmamrikov@gmail.com>
2016-01-21 10:54:48 -0800

    Update mongoose connection code

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/climate_chief");
+mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/climate_chief");
 mongoose.set("debug",true);
 
 module.exports.User = require("./user");
-mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/climate_chief");
