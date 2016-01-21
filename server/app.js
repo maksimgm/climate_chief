b5a223fdd09d6a4e9fea24b6f5f6aaeb78b6b3d5
maksimgm <maksimmamrikov@gmail.com>
2016-01-21 10:25:44 -0800

    add heroku procfile

--- server/app.js
+++ server/app.js
@@ -21,6 +21,6 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
 
-app.listen(3000, function(){
+app.listen((process.env.PORT || 3000), function(){
   console.log("Server is listening on port 3000");
 });
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/satauth");
+mongoose.connect("mongodb://localhost/climate_chief");
 mongoose.set("debug",true);
 
 module.exports.User = require("./user");
+mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/climate_chief");
